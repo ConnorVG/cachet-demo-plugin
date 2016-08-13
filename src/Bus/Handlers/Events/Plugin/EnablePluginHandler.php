c4f2f496dce1806548c0f634fd921c4c29ab3b18
Connor S. Parks <connor@connorvg.tv>
2016-08-13 18:02:25 +0100

    fixes some mong issues

--- src/Bus/Handlers/Events/Plugin/EnablePluginHandler.php
+++ src/Bus/Handlers/Events/Plugin/EnablePluginHandler.php
@@ -2,24 +2,24 @@
 
 namespace ConnorVG\CachetDemoPlugin\Bus\Handlers\Events\Plugin;
 
-use CachetHQ\Cachet\Bus\Events\Plugin\PluginWasEnabledHandler;
+use CachetHQ\Cachet\Bus\Events\Plugin\PluginWasEnabledEvent;
 
 class EnablePluginHandler
 {
     /**
      * Handle the event.
      *
-     * @param \CachetHQ\Cachet\Bus\Events\Plugin\PluginWasEnabledHandler $event
+     * @param \CachetHQ\Cachet\Bus\Events\Plugin\PluginWasEnabledEvent $event
      *
      * @return void
      */
-    public function handle(PluginWasEnabledHandler $event)
+    public function handle(PluginWasEnabledEvent $event)
     {
         // We're only interested in this plugin.
         if ($event->plugin->name !== 'connorvg/cachet-demo-plugin') {
             return;
         }
 
-        // do whatever setup is required
+        // ...
     }
 }
--- src/Bus/Handlers/Events/Plugin/WillDisablePluginHandler.php
+++ src/Bus/Handlers/Events/Plugin/WillDisablePluginHandler.php
@@ -2,24 +2,24 @@
 
 namespace ConnorVG\CachetDemoPlugin\Bus\Handlers\Events\Plugin;
 
-use CachetHQ\Cachet\Bus\Events\Plugin\PluginWasWillDisableHandler;
+use CachetHQ\Cachet\Bus\Events\Plugin\PluginWillBeDisabledEvent;
 
 class WillDisablePluginHandler
 {
     /**
      * Handle the event.
      *
-     * @param \CachetHQ\Cachet\Bus\Events\Plugin\PluginWasWillDisableHandler $event
+     * @param \CachetHQ\Cachet\Bus\Events\Plugin\PluginWillBeDisabledEvent $event
      *
      * @return void
      */
-    public function handle(PluginWasWillDisableHandler $event)
+    public function handle(PluginWillBeDisabledEvent $event)
     {
         // We're only interested in this plugin.
         if ($event->plugin->name !== 'connorvg/cachet-demo-plugin') {
             return;
         }
 
-        // do whatever cleanup is required
+        // ...
     }
 }
